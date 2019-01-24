7f2405a5173f1607c6e706e4b8e14f5ded6d75e9
valero <valero1120@gmail.com>
2019-01-24 17:43:18 -0500

    WIP Module Library

---
+++ library/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Library Management",
+
+    'summary': """
+        Library Management
+        """,
+
+    'author': "OEC_JV",
+    'website': "https://www.odooerpcloud.com",
+
+    'category': 'Tools',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        # 'views/templates.xml',
+    ],
+    'installable': True,
+}
